12d6a10d7e44c8192afd709191f505a47d5445d0
chishaxie <cggde163@163.com>
2013-11-30 00:44:50 -0800

    prepare to support redis client

---
+++ extensions/redis/cocoflow-redis.cc
@@ -0,0 +1,10 @@
+#include "cocoflow-comm.h"
+#include "cocoflow-redis.h"
+
+extern "C" {
+#include "adapters/libuv.h"
+}
+
+namespace ccf {
+
+}
---
+++ include/cocoflow-redis.h
@@ -0,0 +1,10 @@
+#ifndef __COCOFLOW_REDIS_H__
+#define __COCOFLOW_REDIS_H__
+
+#include "cocoflow.h"
+
+namespace ccf {
+
+} /* end of namespace ccf */
+
+#endif
